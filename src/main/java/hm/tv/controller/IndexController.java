bf7b0978def2411eb76ff2859d4e7d7e6e7472c0
791964757@qq.com <gaoerfu2021>
2022-05-11 10:47:14 +0800

    commit

--- src/main/java/hm/tv/controller/IndexController.java
+++ src/main/java/hm/tv/controller/IndexController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -69,7 +71,24 @@ public class IndexController {
     @GetMapping("/link/{id}")
     public String link(@PathVariable String id) {
         try {
-            String text = Jsoup.connect("http://70zhibo.com/api/web/match/" + id).ignoreContentType(true).get().text();
+            Map map = new HashMap<>();
+            map.put("accept"," application/json, text/plain, */*");
+            map.put("accept-encoding"," gzip, deflate, br");
+            map.put("accept-language"," zh-CN,zh;q=0.9");
+            map.put("referer"," https://70zhibo.com/");
+            map.put("sec-ch-ua","\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"97\", \"Chromium\";v=\"97\"");
+            map.put("sec-ch-ua-mobile"," ?0");
+            map.put("sec-ch-ua-platform"," \"Windows\"");
+            map.put("sec-fetch-dest"," empty");
+            map.put("sec-fetch-mode"," cors");
+            map.put("sec-fetch-site"," same-origin");
+            map.put("user-agent"," Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36");
+
+
+            Map cookies = new HashMap<>();
+            cookies.put("Hm_lvt_3b6bc1de9ef0e8981c815510dc8a5d18","1652150030,1652150053,1652154263,1652236174");
+            cookies.put("Hm_lpvt_3b6bc1de9ef0e8981c815510dc8a5d18","1652236382");
+            String text = Jsoup.connect("http://70zhibo.com/api/web/match/" + id).headers(map).cookies(cookies).ignoreContentType(true).get().text();
             JSONObject object = JSON.parseObject(text);
             JSONArray lives = object.getJSONArray("lives");
             if (lives.size() > 0) {
